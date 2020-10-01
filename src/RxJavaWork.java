848e3688ff594f268ef0cfbe1026ac3ec07c4b60
Saranya_Shanmugam3 <saranya_shanmugam3@dell.com>
2020-10-01 11:33:44 -0500

    Renamed the class

--- src/RxJavaWork.java
+++ src/RxJavaWork.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RxJavaTest {
+public class RxJavaWork {
 
     public static void main(String[] args) {
 
